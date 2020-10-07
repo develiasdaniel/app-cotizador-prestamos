c5fd145fb1bd72802aa11f8dacedf78c21f4cc76
Daniel Sanchez <eliasdanielsanchezlopez@gmail.com>
2020-10-06 21:09:31 -0500

    add form with interest, quantity inputs

---
+++ src/components/Form.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {StyleSheet, TextInput, View} from 'react-native';
+import colors from "../utils/colors";
+
+
+export default function Form() {
+    return(
+        <View style={styles.viewForm}>
+            <View style={styles.ViewInputs}>
+                <TextInput 
+                    placeholder="Cantidad a pedir" 
+                    keyboardType="numeric"
+                    style={styles.input}
+                />
+                <TextInput 
+                    placeholder="Interes %" 
+                    keyboardType="numeric"
+                    style={[styles.input, styles.inputPercentaje]}
+                />
+            </View>            
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    viewForm: {
+        position: 'absolute',
+        bottom: -90,
+        width: '85%',
+        paddingHorizontal: 50,
+        backgroundColor: colors.PRIMARY_COLOR_DARK,
+        borderRadius: 30,
+        height: 180,
+        justifyContent: 'center',
+    },
+    ViewInputs: {
+        flexDirection: 'row',
+    },
+    input: {
+        height: 50,
+        backgroundColor: '#fff',
+        borderWidth: 1,
+        borderColor: colors.PRIMARY_COLOR,
+        borderRadius: 7,
+        width: '60%',
+        marginRight: 5,
+        marginLeft: -5,
+        marginBottom: 10,
+        color: '#000', 
+        paddingHorizontal: 10
+    },
+    inputPercentaje:{
+        width: '40%',
+        marginLeft: 5,
+    }
+});
